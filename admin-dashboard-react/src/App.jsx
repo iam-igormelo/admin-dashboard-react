787defaaea46bd1c5f3ecdeaed86f839f7c35080
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2025-12-24 05:22:48 -0300

    Aplicando os estilos ao Header

--- admin-dashboard-react/src/App.jsx
+++ admin-dashboard-react/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function App() {
 
   return (
     <div className='grid-container'>
-
+      <Header />
+      <Sidebar />
+      <Home />
     </div>
   )
 }
